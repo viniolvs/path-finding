7cdedcde8e8e14f8d97ba2f46e7237a82b7587c6
Vinicius de Oliveira Silva <vinioliveira.s@hotmail.com>
2022-12-05 21:14:37 -0300

    Generating labirinth

--- src/Labirinth.java
+++ src/Labirinth.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+
 public class Labirinth {
     private int[][] L;
     private int size;
@@ -5,6 +7,23 @@ public class Labirinth {
     public Labirinth(int size){
         this.size = size;
         L = new int[size][size];
+        L = genLabirinth();
+    }
+
+    private int[][] genLabirinth() {
+        int[][] lab = new int[size][size];
+        Random random = new Random();
+        int[] start = new int[2];
+        int[] end = new int[2];
+        int TOP = 0, BOTTOM = 1, LEFT = 2, RIGHT = 3;
+        
+        start[0] = random.nextInt(size-1);
+        start[1] = random.nextInt(3);
+        end[0] = random.nextInt(size-1);
+        end[1] = random.nextInt(3);
+
+        System.out.println(start[0] + " " + start[1] + " " + end[0] + " " + end[1]);
+        return lab;
     }
 
     public int[][] getLabirinth() {
