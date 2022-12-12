e8267bb4709a18be75b9c471817a6c7fc07f995d
Vinícius <vinioliveira.s@hotmail.com>
2022-12-12 18:44:21 -0300

    A* implemented

--- src/App.java
+++ src/App.java
@@ -1,10 +1,19 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        Labirinth L = new Labirinth(10);
+        Labirinth L = new Labirinth(5);
+        int h1, h2;
+        System.out.println("LABIRINTO");
         L.printLabirinth();
-        L.setPathFinder(new Astar(L));
-        L.findPath();
+        System.out.println("\nCaminho encontrado pela busca em profundidade");
+        h1 = L.findPath();
         L.printPath();
-        System.out.println();
+        System.out.println("Heurística = " + h1);
+        
+        L.restartPath();
+        System.out.println("\nCaminho encontrado pelo A*");
+        L.setPathFinder(new Astar(L));
+        h2 = L.findPath();
+        L.printPath();
+        System.out.println("Heurística = " + h2);
     }
 }
--- src/Astar.java
+++ src/Astar.java
@@ -10,82 +10,80 @@ public class Astar extends PathFinder {
         this.start = L.getStart();
         this.finish = L.getFinish();
         this.len = L.getLen();
-        // Copia o labirinto para o objeto PathFinder
-        this.L = new char[len][len];
-        for (int i = 0; i < len; i++) {
-            for (int j = 0; j < len; j++) {
-                this.L[i][j] = L.getLabirinth()[i][j];
-            }
-        }
+        this.L = L.getPath();
     }
 
     public char[][] getPath() {
         return L;
     }
     
-    // Implementação do método A*
-
-    //calcula a distancia entre ponto atual e ponto final
-    //calcula custo g, custo h e custo f dos pontos ao redor que podem ser seguidos
-    //custo g = distância entre um ponto e o ponto inicial
-    //custo h = distância entre um ponto e o ponto final
-    //custo f = custo g + custo h 
-    //segue o caminho com o menor custo f, caso custo f seja igual, segue o caminho com menor custo h
-    //cria uma lista com a sequencia de pontos seguidos por cada caminho, ao encontrar o ponto final, atribui a lista ao atributo da classe
-    private void aStar(Point p, LinkedList<Point> path) {
-        if (p.getX() != finish.getX() && p.getY() != finish.getY()) {
-            LinkedList<Point> temp = Point.getNeighbors(p);
-            // Elimina os pontos que não podem ser acessados e calcula o custo f para os pontos válidos
-            for (Point point : temp) {
-                if (point.getX() >= 0 && point.getX() < len && point.getY() >= 0 && point.getY() < len
-                        && (L[point.getX()][point.getY()] == ' ' || L[point.getX()][point.getY()] == 's'))
-                    point.setfCost(fCost(point));
-                else
-                    temp.remove(point);
-            }
-            path.addAll(temp);
-            // Procura o próximo ponto com menor custo f
-            Point next = path.get(getNextIndex(path));
-            // Marca o próximo ponto no labirinto
-            L[next.getX()][next.getY()] = 'o';
-            // Continua para o próximo ponto
-            aStar(next, path);
-        }
-    }
-
+    
     public int find() {
         aStar(start, new LinkedList<Point>());
         int heuristica = 0;
         for (int i = 0; i < len; i++) {
             for (int j = 0; j < len; j++) {
                 if (L[i][j] == 'o')
-                    heuristica++;
+                heuristica++;
             }
         }
         return heuristica;
     }
+    
+    // Implementação do método A*
+    private void aStar(Point p, LinkedList<Point> path) {
+        // Marca o caminho no labirinto
+        L[p.getX()][p.getY()] = 'o';
+        // Verifica se encontrou a saída
+        if (p.getX() == finish.getX() && p.getY() == finish.getY())
+            return;
 
+        LinkedList<Point> temp = new LinkedList<Point>();
+        temp = Point.getNeighbors(p);
+        // Elimina os pontos que não podem ser acessados e calcula o custo f para os pontos válidos
+        for (int i = 0; i < temp.size(); i++) {
+            if (temp.get(i).getX() >= 0 && temp.get(i).getX() < len && temp.get(i).getY() >= 0 && temp.get(i).getY() < len
+            && (L[temp.get(i).getX()][temp.get(i).getY()] == ' ' || L[temp.get(i).getX()][temp.get(i).getY()] == 's')){
+                temp.get(i).setfCost(fCost(temp.get(i)));
+                path.push(temp.get(i));
+            }
+        }
+        if(path.size() > 0){
+            // Procura o próximo ponto com menor custo f
+            Point next = path.get(getNextIndex(path));
+            // Remove o ponto atual da lista
+            path.remove(p);
+            // Continua para o próximo ponto
+            aStar(next, path);
+        }
+    }
+
+    // Procura pelo ponto com menor custo f na lista 
     private int getNextIndex(LinkedList<Point> path){
         int minor = 0;
         for (int i = 1; i < path.size(); i++) {
             if (path.get(i).getfCost() < path.get(minor).getfCost())
-                minor = i;
+            minor = i;
         }
         return minor;
     }
 
+    // Custo f = custo g + custo h
     private double fCost(Point p) {
         return (gCost(p) + hCost(p));
     }
-
+    
+    // Custo g = distância entre um ponto e o ponto inicial
     private double gCost(Point p) {
         return distance(p, start);
     }
-
+    
+    // Custo h = distância entre um ponto e o ponto final
     private double hCost(Point p) {
         return distance(p, finish);
     }
-
+    
+    // Calcula a distância entre dois pontos
     private double distance(Point p1, Point p2) {
         return Math.sqrt(Math.pow((p2.getX() - p1.getX()), 2) + Math.pow((p2.getY() - p1.getY()), 2));
     }
